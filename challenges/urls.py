b752696e79ea7ab023e88b49dad7ee88424163df
Nick Spencer <strangebrew202@gmail.com>
2024-03-26 09:17:55 -0600

    Add: learning set up for Django templating engine

--- challenges/urls.py
+++ challenges/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path("hello-world", views.index),
+    path("", views.index), # /challenges/
+    path("<int:month>", views.monthly_challenge_by_number),
+    path("<str:month>", views.monthly_challenge, name="month-challenge")
 ]
